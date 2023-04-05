8def955658e614c8ba7f0e427efbc6b0268f16e1
MotivatedUser <carbfixer@gmail.com>
2023-04-05 06:23:04 -0700

    Router to HashRouter

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm'
-import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const ContactPage = () => {
