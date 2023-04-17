da9b779d63e55a570849307d1a50a5d36f8c2cd8
MotivatedUser <carbfixer@gmail.com>
2023-04-17 14:17:06 -0700

    Adds loading animation with CSS

--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import { projectsData } from './ProjectsData'
-import ProjectItem from './ProjectItem'
+import React, { useState, useEffect } from 'react';
+import { projectsData } from './ProjectsData';
+import ProjectItem from './ProjectItem';
 import Search from './Search';
 
 function ProjectsGrid() {
   const [isLoading, setIsLoading] = useState(true);
   const [projects, setProjects] = useState([]);
-  const [query, setQuery] = useState("")
+  const [query, setQuery] = useState('');
 
   useEffect(() => {
     // Simulate loading time and only allows 12 results to show at 1 time.
@@ -17,22 +17,28 @@ function ProjectsGrid() {
     }, 1000);
   }, []);
 
-  const filteredProjects = projects.filter(project =>
+  const filteredProjects = projects.filter((project) =>
     project.title.toLowerCase().includes(query.toLowerCase())
   );
 
-  return isLoading ? (
-    <h1>Loading...</h1>
-  ) : (
+  return (
     <>
-    <Search getQuery={(q) => setQuery(q)} />
-    <div className="cards">
-      {filteredProjects.map((project, index) => (
-        <ProjectItem project={project} key={index} />
-      ))}
-    </div>
+      <Search getQuery={(q) => setQuery(q)} />
+      {isLoading ? (
+        <div className="d-flex justify-content-center align-items-center spinner-wrapper" style={{ paddingTop: '10%' }}>
+          <div className="spinner-border orange" role="status" >
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : (
+        <div className="cards">
+          {filteredProjects.map((project, index) => (
+            <ProjectItem project={project} key={index} />
+          ))}
+        </div>
+      )}
     </>
   );
 }
 
-export default ProjectsGrid
+export default ProjectsGrid;
