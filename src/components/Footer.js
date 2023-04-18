e5027befc47d6690398e2a17abe16be76356a79b
MotivatedUser <carbfixer@gmail.com>
2023-04-18 08:30:39 -0700

    Adds Contrast, aria-labels on links and h5 to h3 in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,59 +14,86 @@ const Footer = () => {
     <footer className="footer">
       <Container>
         <Row>
-          <Col  sm="4" className="text-center">
-            <h5><strong>Links</strong></h5>
+          <Col sm="4" className="text-center">
+            <h3>
+              <strong>Links</strong>
+            </h3>
             <ul className="list-unstyled">
               <li>
-                <Link className="blue" to="/">Home</Link>
+                <Link className="blue" to="/">
+                  Home
+                </Link>
               </li>
               <li>
-                <Link className="blue" to="/about">About</Link>
+                <Link className="blue" to="/about">
+                  About
+                </Link>
               </li>
               <li>
-                <Link className="blue" to="/projects">Projects</Link>
+                <Link className="blue" to="/projects">
+                  Projects
+                </Link>
               </li>
               <li>
-                <Link className="blue" to="/resume">Resume</Link>
+                <Link className="blue" to="/resume">
+                  Resume
+                </Link>
               </li>
               <li>
-                <Link className="blue" to="/skills">Skills</Link>
+                <Link className="blue" to="/skills">
+                  Skills
+                </Link>
               </li>
               <li>
-                <Link className="blue" to="/contact">Contact</Link>
+                <Link className="blue" to="/contact">
+                  Contact
+                </Link>
               </li>
             </ul>
           </Col>
           <Col sm="4" className="text-center">
-            <h5 className="footer-h5" ><strong>Social</strong></h5>
+            <h3 className="footer-h3">
+              <strong>Social</strong>
+            </h3>
             <a
               className="btn btn-social-icon"
               href="https://github.com/MotivatedUser"
+              aria-label="GitHub"
             >
               <FaGithubSquare className="footer-icon" />
             </a>
             <a
               className="btn btn-social-icon"
               href="https://www.linkedin.com/in/d-altermatt-motivated-user"
+              aria-label="LinkedIn"
             >
               <FaLinkedinIn className="footer-icon blue" />
             </a>
             <a
               className="btn btn-social-icon"
               href="https://twitter.com/altermatt_d"
+              aria-label="Twitter"
             >
               <FaTwitterSquare className="footer-icon blue" />
             </a>
             <a
               className="btn btn-social-icon"
               href="https://discordapp.com/users/1034946698860052530"
+              aria-label="Discord"
             >
               <FaDiscord className="footer-icon" />
             </a>
           </Col>
           <Col sm="4" className="text-center">
-            <h5 className="footer-h5"><strong>Contact</strong></h5>
-            <a role="button" className="btn btn-link footer-link" href="tel:+15094815800">
+            <h3 className="footer-h3">
+              <strong>Contact</strong>
+            </h3>
+            <a
+              role="button"
+              className="btn btn-link footer-link"
+              href="tel:+15094815800"
+              aria-label="Phone"
+            >
               <FcPhone className="blue" /> 1-509-481-5800
             </a>
             <br />
@@ -74,6 +101,7 @@ const Footer = () => {
               role="button"
               className="btn btn-link footer-link"
               href="mailto:reactivedoug@gmail.com"
+              aria-label="Email"
             >
               <FaEnvelope className="blue" /> reactivedoug@gmail.com
             </a>
