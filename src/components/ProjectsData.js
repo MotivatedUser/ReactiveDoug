7c1886cd672fd79ee38138537c3ad3b51a1e5d6d
MotivatedUser <carbfixer@gmail.com>
2023-04-19 13:14:21 -0700

    Adds Flashcard Project to projects and flashcard-image

--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -1,48 +1,56 @@
 export const projectsData = [
-
-    
-    {
-        title: 'GroceryList',
-        image: process.env.PUBLIC_URL + '/images/shopping-list-image.jpg',
-        url: 'https://motivateduser.github.io/GroceryList/',
-        technologies: 'React, React-DOM, @emotion/react, @emotion/styled, @mui/material, @reduxjs/toolkit, React-Redux, React-Icons, React-Scripts, Web-Vitals, GH-Pages.',
-        learning: 'This project enhanced my state management skills with @reduxjs/toolkit, using createSlice and nanoid for unique IDs. I utilized React Hooks for component states and Redux store interactions. I gained experience with Material-UI for a responsive interface and employed @emotion/react and @emotion/styled for component styling and theme customization.'  
-    }
-    ,
-    {
-        title: 'React-menu-app',
-        image: process.env.PUBLIC_URL + '/images/react-menu-app-image.jpg',
-        url: 'https://motivateduser.github.io/react-menu-app/',
-        technologies: 'React, React-DOM, Axios, React-Icons, React-Scripts, Reactstrap, React-Router-DOM, Redux, React-Redux, React-Hook-Form, Bootstrap, CSS, GitHub, and GH-Pages.',
-        learning: 'During this project, I gained experience using Axios for fetching data and managing API requests with async and await, as well as implementing navigation between pages using React-Router-DOM. I employed Redux for state management and leveraged React-Hook-Form for form handling and validation. The application utilizes Bootstrap and Reactstrap for responsive design and styling.'
-    }
-    ,    
-    {
-        title: 'Portfolio Project 2',
-        image: process.env.PUBLIC_URL + '/images/pp2-image.jpg',
-        url: 'https://motivateduser.github.io/pp2/',
-        technologies: 'React, React DOM, React Router, Redux, Formik, React Spring, Bootstrap, Bootstrap Social, CSS, Styled Components, Reactstrap, Font Awesome, React Icons, GitHub and GitHub Pages',
-        learning: 'This project allowed me to build a React web application with additional libraries such as Redux, React Router, and Formik. I also learned how to use React Spring for animation effects and Bootstrap, Reactstrap, and Styled Components for styling. Finally, I gained experience in deploying the app to GitHub Pages and using GitHub for version control.'
-    }
-    ,
-    {
-        title: 'Rick and Morty API Project',
-        image: process.env.PUBLIC_URL + '/images/rick-morty-project-image.jpg',
-        url: 'https://motivateduser.github.io/Rick-Morty-Project/',
-        technologies: 'React, React-DOM, Axios, React-Icons, React-Scripts, Reactstrap, CSS, GitHub and GH-Pages',
-        learning: 'Through this project, I learned about using axios to fetch data, useState and useEffect hooks to manage state and side effects, and async and await to handle asynchronous API requests. I also gained experience with React components, props, and event handling.'
-    }
-    ,
-    {
-        title: 'Random "Funny" Quote Machine',
-        image: process.env.PUBLIC_URL + '/images/quote-machine-image.jpg',
-        url: 'https://motivateduser.github.io/RandomQuoteGenerator/',
-        technologies: 'React, React DOM, Font Awesome, React-Icons, Formik, Bootstrap, React-Bootstrap, Styled-Components, GitHub and GH-pages',
-        learning: 'I gained experience with React hooks, dynamic data rendering, CSS styling, external libraries, event listeners, and component modularization. The project enhanced my React skills and increased my understanding of front-end development concepts.'
-    }    
-]
-
-
-
-
-
+  {
+    title: "Flashcards",
+    image: process.env.PUBLIC_URL + "/images/flashcards-image.jpg",
+    url: "https://motivateduser.github.io/flash-cards/",
+    technologies:
+      "React, React-DOM, React-Redux, @reduxjs/toolkit, React-Scripts, Web-Vitals, GH-Pages",
+    learning:
+      "This project improved my understanding of state management using React Hooks and @reduxjs/toolkit, specifically by leveraging createSlice for handling complex state logic. I focused on enhancing accessibility throughout the application, ensuring that all components are keyboard navigable, properly labeled, and adhere to ARIA best practices. Additionally, I practiced using different flashcard datasets to allow users to choose and switch between categories seamlessly.",
+  },
+  {
+    title: "GroceryList",
+    image: process.env.PUBLIC_URL + "/images/shopping-list-image.jpg",
+    url: "https://motivateduser.github.io/GroceryList/",
+    technologies:
+      "React, React-DOM, @emotion/react, @emotion/styled, @mui/material, @reduxjs/toolkit, React-Redux, React-Icons, React-Scripts, Web-Vitals, GH-Pages.",
+    learning:
+      "This project enhanced my state management skills with @reduxjs/toolkit, using createSlice and nanoid for unique IDs. I utilized React Hooks for component states and Redux store interactions. I gained experience with Material-UI for a responsive interface and employed @emotion/react and @emotion/styled for component styling and theme customization.",
+  },
+  {
+    title: "React-menu-app",
+    image: process.env.PUBLIC_URL + "/images/react-menu-app-image.jpg",
+    url: "https://motivateduser.github.io/react-menu-app/",
+    technologies:
+      "React, React-DOM, Axios, React-Icons, React-Scripts, Reactstrap, React-Router-DOM, Redux, React-Redux, React-Hook-Form, Bootstrap, CSS, GitHub, and GH-Pages.",
+    learning:
+      "During this project, I gained experience using Axios for fetching data and managing API requests with async and await, as well as implementing navigation between pages using React-Router-DOM. I employed Redux for state management and leveraged React-Hook-Form for form handling and validation. The application utilizes Bootstrap and Reactstrap for responsive design and styling.",
+  },
+  {
+    title: "Portfolio Project 2",
+    image: process.env.PUBLIC_URL + "/images/pp2-image.jpg",
+    url: "https://motivateduser.github.io/pp2/",
+    technologies:
+      "React, React DOM, React Router, Redux, Formik, React Spring, Bootstrap, Bootstrap Social, CSS, Styled Components, Reactstrap, Font Awesome, React Icons, GitHub and GitHub Pages",
+    learning:
+      "This project allowed me to build a React web application with additional libraries such as Redux, React Router, and Formik. I also learned how to use React Spring for animation effects and Bootstrap, Reactstrap, and Styled Components for styling. Finally, I gained experience in deploying the app to GitHub Pages and using GitHub for version control.",
+  },
+  {
+    title: "Rick and Morty API Project",
+    image: process.env.PUBLIC_URL + "/images/rick-morty-project-image.jpg",
+    url: "https://motivateduser.github.io/Rick-Morty-Project/",
+    technologies:
+      "React, React-DOM, Axios, React-Icons, React-Scripts, Reactstrap, CSS, GitHub and GH-Pages",
+    learning:
+      "Through this project, I learned about using axios to fetch data, useState and useEffect hooks to manage state and side effects, and async and await to handle asynchronous API requests. I also gained experience with React components, props, and event handling.",
+  },
+  {
+    title: 'Random "Funny" Quote Machine',
+    image: process.env.PUBLIC_URL + "/images/quote-machine-image.jpg",
+    url: "https://motivateduser.github.io/RandomQuoteGenerator/",
+    technologies:
+      "React, React DOM, Font Awesome, React-Icons, Formik, Bootstrap, React-Bootstrap, Styled-Components, GitHub and GH-pages",
+    learning:
+      "I gained experience with React hooks, dynamic data rendering, CSS styling, external libraries, event listeners, and component modularization. The project enhanced my React skills and increased my understanding of front-end development concepts.",
+  },
+];
